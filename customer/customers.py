76db61e1252c4cf9d2ca3961cf65d89c6973f412
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-19 23:51:53 +0100

    Fixed naming error and repr error

--- customer/customers.py
+++ customer/customers.py
@@ -1,13 +1,13 @@
 class Customers():
     def __init__(self):
-        self._customers_list = []
+        self._customer_list = []
 
     @property
     def customer_list(self):
-        return self._customers_list
+        return self._customer_list
 
     def add(self, customer):
-        self._customers_list.append(customer)
+        self._customer_list.append(customer)
 
     def get_customer_by_ppsn(self, ppsn):
         for customer in self.customer_list:
@@ -16,6 +16,17 @@ class Customers():
         return
 
     def __repr__(self):
-        for customer in self.customer_list:
-            print(customer)
-        return
+        result = ""
+        for customer_obj in self._customer_list:
+            customer_str = ""
+            customer_str += "First name: "
+            customer_str += customer_obj.first_name
+            customer_str += ", "
+            customer_str += "Last name: "
+            customer_str += customer_obj.last_name
+            customer_str += ", "
+            customer_str += "PPSN: "
+            customer_str += customer_obj.ppsn
+            customer_str += "\n"
+            result += customer_str
+        return result
