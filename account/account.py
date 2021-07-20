814d9cab5fcdd2d7218a4736fe1b1764fb83b110
miyazakimaiko <myzkmik19922@gmail.com>
2021-07-20 21:56:28 +0100

    Fixed bug not reflecting interest rate

--- account/account.py
+++ account/account.py
@@ -3,8 +3,7 @@ class Account():
     def __init__(self, balance):
         self._balance = balance
         self._interest_rate = 0.02
-        if self._balance > 10000:
-            self._interest_rate = 0.05
+        self.reflect_interest_rate()
 
     @property
     def balance(self):
@@ -20,13 +19,24 @@ class Account():
         return False
 
     def send(self, amount):
-        self._balance = self._balance - amount
+        self._balance = round((self._balance - amount), 2)
+        self.reflect_interest_rate()
 
     def receive(self, amount):
-        self._balance = self._balance + amount
+        self._balance = round((self._balance + amount), 2)
+        self.reflect_interest_rate()
 
     def lodge(self, amount):
-        self._balance = self._balance + amount
+        self._balance = round((self._balance + amount), 2)
+        self.reflect_interest_rate()
+
+    def reflect_interest_rate(self):
+        if self._balance > 10000:
+            self._interest_rate = 0.05
+        else:
+            self._interest_rate = 0.02
+
+
 
 
 class Current(Account):
@@ -50,7 +60,10 @@ class Current(Account):
 
     def withdraw(self, amount):
         if amount <= self._balance or self._overdraft is True:
-            self._balance = self._balance - amount
+            self._balance = round((self._balance - amount), 2)
+            self.reflect_interest_rate()
+            return True
+        return False
 
     def __repr__(self):
         return f"type: current, balance: {self.balance}, interest_rate: {self._interest_rate}, overdtaft: {self.overdraft}"
